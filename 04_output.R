97d741b82e0e9d92eb15450b7d648fb862776487
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-10-05 16:42:07 -0700

    update prov-level plots

--- 04_output.R
+++ 04_output.R
@@ -21,30 +21,7 @@ if (!exists(".header_sourced")) source("header.R")
 if (!exists("results_out"))  load("./tmp/analysis_data.RData")
 
 
-
 ## Provinicial Summaries
-
-## Plot theme
-theme_set(theme_classic() +
-            theme(text = element_text(colour = "black"),
-                  axis.line = element_blank(),
-                  axis.ticks = element_blank(),
-                  panel.grid.major = element_line(colour = "grey85", size = 0.5,
-                                                linetype = 1),
-                  panel.grid.minor = element_line(colour = "grey90", size = 0.5,
-                                                linetype = 1),
-                  panel.grid.major.x = element_blank(),
-                  panel.spacing = unit(0.6, "lines"),
-                  plot.title = element_text(vjust = 2, hjust = 0.5),
-                  axis.title = element_text(vjust = 0.1),
-                  legend.position = "bottom", legend.title = element_blank(),
-                  legend.text = element_text(size = 12),
-                  axis.text.x = element_blank(),
-                  strip.background = element_blank()))
-
-label.colour <- "black" 
-colour.scale <- brewer.pal(3,"Blues")
-
 
 ## Select wells analyzed and create factors
 results_viz <- results_out[results_out$category != "N/A",] %>%
@@ -61,29 +38,78 @@ results_viz <- results_out[results_out$category != "N/A",] %>%
 
 ## Overall summary of trend categories
 
+## Bar chart theme
+theme_barcharts <- theme(
+  axis.text.y = element_text(size = 12),
+  axis.text.x = element_text(size = 12),
+  axis.title = element_blank(), 
+  plot.title = element_text(size = 17, hjust = 0.5),
+  plot.margin = unit(c(6,6,6,2),"mm")
+)
+
+
+## Plot theme
+# theme_set(theme_classic() +
+#             theme(text = element_text(colour = "black"),
+#                   axis.line = element_blank(),
+#                   axis.ticks = element_blank(),
+#                   panel.grid.major = element_line(colour = "grey85", size = 0.5,
+#                                                 linetype = 1),
+#                   panel.grid.minor = element_line(colour = "grey90", size = 0.5,
+#                                                 linetype = 1),
+#                   panel.grid.major.x = element_blank(),
+#                   panel.spacing = unit(0.6, "lines"),
+#                   plot.title = element_text(vjust = 2, hjust = 0.5),
+#                   axis.title = element_text(vjust = 0.1),
+#                   legend.position = "bottom", legend.title = element_blank(),
+#                   legend.text = element_text(size = 12),
+#                   axis.text.x = element_blank(),
+#                   strip.background = element_blank()))
+
+label.colour <- "black" 
+colour.scale <- brewer.pal(3,"Blues")
+
+
 #summary df
 sum_data <- results_viz %>%
   group_by(category) %>%
-  summarise(Freq = n()) %>%
+  summarise(frequency = n()) %>%
   arrange(desc(category)) %>%
-  mutate(per = round(Freq/sum(Freq)*100),
-         pos = cumsum(Freq) - Freq/2)
+  mutate(percent = round(frequency/sum(frequency), digits = 2),
+         position = cumsum(percent) - percent/2,
+         geography = "British Columbia")
+
+#bar chart of category summary
+(bc_bar_chart <- ggplot(sum_data, aes(x = geography, y = percent, fill = category)) +
+  geom_bar(stat = "identity", alpha = 0.7) +
+  scale_fill_manual(values = colour.scale) +
+  geom_text(aes(y = position, label = paste0(frequency, " wells")),
+             colour = label.colour) +
+    scale_y_continuous(expand = c(0,0), labels = percent) +
+  labs(y = "Percent of Groundwater Wells", x = " ") +
+    theme_soe() +
+    theme_barcharts +
+    theme(panel.grid.major.x = element_blank(),
+          legend.position = "none",
+          axis.text.x = element_text(size = 14))
+)
+
 
 #pie chart of category summary
-(pie_plot <- ggplot(results_viz, aes(x = factor(1), fill = category)) + 
-  geom_bar(width = 1) + coord_polar(start = 0, theta = "y") + 
-  scale_fill_manual(values = colour.scale) + 
-  theme(line = element_blank(), axis.text = element_blank(),
-        axis.title = element_blank(), plot.title = element_text(vjust = 0),
-        legend.position = c(0.5,0.01), legend.direction = "horizontal",
-        legend.title = element_blank(), 
-    #    plot.margin = unit(c(rep(0,4)), "cm"),
-        panel.grid.minor = element_blank(), panel.grid.major = element_blank()) + 
-  geom_text(data = sum_data,
-            aes(x = 1.2, y = pos, label = paste0(per,"%")),
-            colour = label.colour) + 
-  ggtitle("Percentage of Groundwater Wells in Three Different\nCategories of Long-term Trends in Groundwater Levels")
-)
+# (pie_plot <- ggplot(results_viz, aes(x = factor(1), fill = category)) + 
+#   geom_bar(width = 1) + coord_polar(start = 0, theta = "y") + 
+#   scale_fill_manual(values = colour.scale) + 
+#   theme(line = element_blank(), axis.text = element_blank(),
+#         axis.title = element_blank(), plot.title = element_text(vjust = 0),
+#         legend.position = c(0.5,0.01), legend.direction = "horizontal",
+#         legend.title = element_blank(), 
+#     #    plot.margin = unit(c(rep(0,4)), "cm"),
+#         panel.grid.minor = element_blank(), panel.grid.major = element_blank()) + 
+#   geom_text(data = sum_data,
+#             aes(x = 1.2, y = pos, label = paste0(per,"%")),
+#             colour = label.colour) + 
+#   ggtitle("Percentage of Groundwater Wells in Three Different\nCategories of Long-term Trends in Groundwater Levels")
+# )
 
 ## Summary of categories by region
 
@@ -97,99 +123,106 @@ nLabeller <- function(n, singular, sep = " ") {
 #regional summary df
 sum_data_reg <- results_viz %>%
   group_by(REGION_NAME, category) %>%
-  summarise(Freq = n()) %>%
-  mutate(prop = Freq/sum(Freq), 
+  summarise(frequency = n()) %>%
+  mutate(proportion = frequency/sum(frequency), 
          region_lab = paste0(gsub("(\\s)","\\\n", 
                                   gsub("\\s/\\s*", "/\\\n", REGION_NAME)), 
-                             "\n(", nLabeller(sum(Freq), "well"), ")"))
+                             "\n(", nLabeller(sum(frequency), "well"), ")")) %>% 
+  complete(nesting(REGION_NAME, region_lab), category, fill = list(frequency = 0, proportion = 0))
+
 
 #bar chart plot with percentage on y and sample size labels
-(regional_plot <- ggplot(sum_data_reg, aes(x = category, y = prop, fill = category)) + 
-    geom_bar(stat = 'identity') + facet_grid(~ region_lab) + 
-    labs(title = "Trends in Groundwater Levels by Region", 
-         x = element_blank(), y = "Percent of Groundwater Wells") + 
-    scale_fill_manual(values = colour.scale) +
-    scale_y_continuous(labels = percent, limits = c(0,1)) +
-    theme(axis.text.y = element_text(colour = "black"),
-          strip.text = element_text(colour = "black", size = 9))
+(regional_bar_chart <- ggplot(sum_data_reg,
+                              aes(x = fct_reorder2(region_lab, category, proportion), y = proportion, fill = category)) + 
+    geom_bar(stat = 'identity', alpha = 0.7) +
+    coord_flip() +
+    scale_fill_manual(name = "Groundwater Level\nTrend Category:", values = colour.scale) +
+    scale_y_continuous(labels = percent, expand = c(0,0)) +
+    theme_soe() +
+    theme_barcharts +
+    theme(panel.grid.major.y = element_blank(),
+          axis.title.y = element_blank(),
+          legend.text = element_text(size = 14),
+          legend.title = element_text(size = 16),
+          legend.position = "right")
 )
 
 
-## Summarize by aquifer type
-
-#aquifer type df
-sum_data_aq <- results_viz %>% 
-  filter(AQUIFER_TYPE != "Unknown") %>%
-  group_by(AQUIFER_TYPE, category) %>%
-  summarise(Freq = n()) %>%
-  mutate(prop = Freq/sum(Freq),
-         aq_lab = paste0(AQUIFER_TYPE, "\n(",
-                         nLabeller(sum(Freq), "well"), ")"))
-
-#bar chart plot with percentage on y and sample size labels
-(aq_plot <- ggplot(sum_data_aq, aes(x = category, y = prop, fill = category)) +
-    geom_bar(stat = 'identity') + facet_grid(~ aq_lab) +
-    labs(title = "Trends in Groundwater Levels by Aquifer Type",
-         x = element_blank(), y = "Percent of Groundwater Wells") +
-    scale_fill_manual(values = colour.scale) +
-    scale_y_continuous(labels = percent, limits = c(0,1)) +
-    theme(axis.text.y = element_text(colour = "black"),
-          strip.text = element_text(colour = "black", size = 9))
-)
+# ## Summarize by aquifer type
+# 
+# #aquifer type df
+# sum_data_aq <- results_viz %>% 
+#   filter(AQUIFER_TYPE != "Unknown") %>%
+#   group_by(AQUIFER_TYPE, category) %>%
+#   summarise(Freq = n()) %>%
+#   mutate(prop = Freq/sum(Freq),
+#          aq_lab = paste0(AQUIFER_TYPE, "\n(",
+#                          nLabeller(sum(Freq), "well"), ")"))
+# 
+# #bar chart plot with percentage on y and sample size labels
+# (aq_plot <- ggplot(sum_data_aq, aes(x = category, y = prop, fill = category)) +
+#     geom_bar(stat = 'identity') + facet_grid(~ aq_lab) +
+#     labs(title = "Trends in Groundwater Levels by Aquifer Type",
+#          x = element_blank(), y = "Percent of Groundwater Wells") +
+#     scale_fill_manual(values = colour.scale) +
+#     scale_y_continuous(labels = percent, limits = c(0,1)) +
+#     theme(axis.text.y = element_text(colour = "black"),
+#           strip.text = element_text(colour = "black", size = 9))
+# )
 
 
 ## Save plot objects to tmp folder
-save(pie_plot, regional_plot, aq_plot, file = "tmp/figures.RData")
-
+# save(pie_plot, regional_plot, aq_plot, file = "tmp/figures.RData")
+save(bc_bar_chart, regional_bar_chart, file = "tmp/figures.RData")
 
 ## Save plots as high resolution PNG/SVGs for web
-pie <- "out/figs/status_pie"
+status.bc <- "out/figs/status-bc"
 status.reg <- "out/figs/status-by-reg"
-status.aq <- "out/figs/status-by-aq"
-status.reg.aq <- "out/figs/status-by-reg-aq"
+# status.aq <- "out/figs/status-by-aq"
+status.reg.bc <- "out/figs/status-by-reg-bc"
 
-#pie chart
-png_retina(glue(pie, ".png"), width = 440, height = 400)
-plot(pie_plot)
+#bc bar chart
+png_retina(glue(status.bc, ".png"), width = 500, height = 500)
+plot(bc_bar_chart)
 dev.off()
 
-svg_px(glue(pie, ".svg"), width = 440, height = 400)
-plot(pie_plot)
+svg_px(glue(status.bc, ".svg"), width = 500, height = 500)
+plot(bc_bar_chart)
 dev.off()
 
 #regional bar chart
-png_retina(glue(status.reg, ".png"), width = 800, height = 400)
-plot(regional_plot)
+png_retina(glue(status.reg, ".png"), width = 500, height = 500)
+plot(regional_bar_chart)
 dev.off()
 
-svg_px(glue(status.reg, ".svg"), width = 800, height = 400)
-plot(regional_plot)
+svg_px(glue(status.reg, ".svg"), width = 500, height = 500)
+plot(regional_bar_chart)
 dev.off()
 
 
-#aquifer type bar chart
-png_retina(glue(status.aq, ".png"), width = 440, height = 400)
-plot(aq_plot)
-dev.off()
-
-svg_px(glue(status.aq, ".svg"), width = 440, height = 400)
-plot(aq_plot)
-dev.off()
+# #aquifer type bar chart
+# png_retina(glue(status.aq, ".png"), width = 440, height = 400)
+# plot(aq_plot)
+# dev.off()
+# 
+# svg_px(glue(status.aq, ".svg"), width = 440, height = 400)
+# plot(aq_plot)
+# dev.off()
 
 #bar charts combined using grid.arrange()
-png_retina(glue(status.reg.aq, ".png"), width = 930, height = 330)
-grid.arrange(regional_plot + theme(legend.position = "none"),
-             aq_plot + theme(legend.position = "none"),
-             ncol = 2, widths = c(3,2))
+png_retina(glue(status.reg.bc, ".png"), width = 930, height = 500)
+grid.arrange(bc_bar_chart,
+              regional_bar_chart + theme(legend.position = "none"),
+             ncol = 2, widths = c(2,4))
 dev.off()
 
-svg_px(glue(status.reg.aq, ".svg"), width = 930, height = 330)
-grid.arrange(regional_plot + theme(legend.position = "none"),
-             aq_plot + theme(legend.position = "none"),
-             ncol = 2, widths = c(3,2))
+svg_px(glue(status.reg.bc, ".svg"), width = 930, height = 500)
+grid.arrange(bc_bar_chart,
+             regional_bar_chart + theme(legend.position = "none"),
+             ncol = 2, widths = c(2,3))
 dev.off()
 
-
+## use cowplot::get_legend to plaot legend under both bar charts in one object
 
 ## Map Summary (for PDF print version)
 
