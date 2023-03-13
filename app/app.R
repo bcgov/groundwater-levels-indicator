cd855ce1c61b64d117d2de56003529ca5a032794
Ekaterina.Daviel <ekaterina@eclipsegeomatics.com>
2023-03-13 15:28:14 -0700

    updated shiny apps following Mar 6 2023 discussion

--- app/app.R
+++ app/app.R
@@ -15,8 +15,7 @@ library(sf)
 source('functions.R')
 
 #Read in data, filter out uncategorized wells
-input_dataframe <- read.csv("data/gw_well_results.csv") %>%
-  filter(., state !="Too many missing observations to perform trend analysis" & state !="Recently established well; time series too short for trend analysis" )
+input_dataframe <- read.csv("data/gw_well_results.csv")
 
 # Define UI for application
 ui <- fluidPage(
@@ -25,17 +24,16 @@ ui <- fluidPage(
 
     column(3,
            radioButtons(inputId = "user_period_choice", label = "Time Range",
-                        choices = c("All Data" = "All", "Last 10 Years (2012-2022)" = "10 Years"), selected = "All")),
+                        choices = c("All Data" = "All", "Last 10 Years (2012-2022)" = "10 Years",
+                                    "Last 20 Years (2002-2022)" = "20 Years"), selected = "All")),
 
     column(3,
            selectizeInput(inputId = "user_var_choice", label = "Metric to Display",
-                          choices = c("Mean Annual" = "Mean", "Minimum Annual" = "Minimum"), selected = "Mean")),
+                          choices = c("Mean Annual" = "Yearly", "Mean Monthly" = "Monthly"), selected = "Yearly")),
 
     column(3,
-           radioButtons(inputId = "time_scale", label = "Yearly or Monthly Data",
-                        choices = c("Yearly", "Monthly"), selected = "Yearly")),
-    column(3,
            uiOutput("month_selector_UI")),
+
 
   ),
 
@@ -49,6 +47,7 @@ ui <- fluidPage(
 
   )
 
+
 )
 
 
@@ -57,23 +56,9 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
 
-  # Update month selector to show months, if user picks month time-scale
-  observeEvent(input$time_scale, {
-    if(input$time_scale == 'Monthly'){
-      updateSelectizeInput(inputId = 'user_var_choice',
-                           choices = c("Monthly Median" = "Mean")
-      )
-    }
-    if(input$time_scale == 'Yearly'){
-      updateSelectizeInput(inputId = 'user_var_choice',
-                           choices = c("Mean Annual" = "Mean", "Minimum Annual" = "Minimum")
-      )
-    }
-  })
-
-
+  #Define reactive UI for months based on monthly mean selection
   output$month_selector_UI = renderUI({
-    if(input$time_scale == 'Yearly') return(NULL)
+    if(input$user_var_choice == 'Yearly') return(NULL)
     selectizeInput(inputId = 'month_selector',
                    label = 'Month',
                    multiple = F,
@@ -81,15 +66,46 @@ server <- function(input, output) {
                    selected = month.abb[1])
   })
 
+  #Split input data set to speed up processing?
+  # monthly_data <- input_dataframe %>%
+  #   filter(time_scale == "Monthly")
+  # yearly_data <- input_dataframe %>%
+  #   filter(time_scale == "Yearly")
+
  #Filter dataset by input variables
   filtered_data <- reactive({
 
+    if(input$user_var_choice == 'Yearly'){
     filter(input_dataframe,
-           period == input$user_period_choice,
-           metric == input$user_var_choice,
-           time_scale == input$time_scale)
+           time_scale == input$user_var_choice,
+           period == input$user_period_choice)}
+
+    else{
+     filter(input_dataframe,
+            time_scale == input$user_var_choice,
+             period == input$user_period_choice,
+            month == input$month_selector)}
 
   })
+
+  month <- reactiveVal('Yearly')
+
+  observeEvent(input$month_selector, {
+
+  if(input$user_var_choice == 'Monthly'){
+    filter(input_dataframe,
+           period == input$user_period_choice,
+           time_scale == input$user_var_choice,
+           month == input$month_selector)
+
+    month_in <- paste0(input$month_selector)
+    month(month_in)
+    }
+
+    #output$text <- renderText({paste0(month())})
+
+  })
+
 
 
   output$summary_plot <- renderPlot({
--- app/functions.R
+++ app/functions.R
@@ -13,6 +13,11 @@
 #Define function to create regional summary plot
 prov_summary_plot <- function(data){
 
+  #Filter out wells with no state
+  data <- data %>%
+    filter(., state !="Too many missing observations to perform trend analysis" &
+             state !="Recently established well; time series too short for trend analysis" )
+
 #Count the number of wells in each state and calculate the respective proportions
 input_summary <- data %>%
   group_by(state) %>%
@@ -25,7 +30,7 @@ input_summary <- data %>%
   )) %>%
   mutate(total_no_wells = nrow(data)) %>%
   mutate(prop = (count/total_no_wells)*100) %>%
-  mutate("no_wells_lab" = paste0(count, " wells")) %>%
+  mutate("no_wells_lab" = ifelse(count>1, paste0(count, " wells"), paste0(count, " well"))) %>%
   mutate(label_x = cumsum(count)) #Calculate the total count of wells for bar graph label
 
 #Create field for barplot labels
@@ -47,7 +52,7 @@ barlab=levels(as.factor(input_summary$state ))
 #   input_summary <- input_summary %>%
 #     mutate(label_x = cumsum(count))
 
-#Create provincial summary bargraph
+#Create provincial summary bar graph
 ggplot(data=input_summary) +
   geom_col(mapping=aes(x=prop, y=state, fill=state, width = 0.5)) +
   scale_fill_manual(label=barlab, values=barcol) +
@@ -55,7 +60,7 @@ ggplot(data=input_summary) +
   scale_x_continuous(expand = c(0,0)) +
   expand_limits(x=c(0,105)) +
   guides(fill = guide_legend(reverse = TRUE, nrow=2))+
-  labs(title = "Provincial Summary") +
+  #labs(title = "Groundwater Wells by State") +
   xlab("Proportion of wells (%)") + ylab(NULL) +
   theme_classic() +
   #theme(legend.position="bottom",
@@ -66,6 +71,12 @@ ggplot(data=input_summary) +
 ##########################################################
 #Define function to create regional summary plot
 regional_summary_plot <- function(data){
+
+  #Filter out wells with no state
+  data <- data %>%
+    filter(., state !="Too many missing observations to perform trend analysis" &
+             state !="Recently established well; time series too short for trend analysis" )
+
 
 #Summarize results by region and count wells in each state
 input_regional <- data %>%
@@ -82,7 +93,7 @@ input_regional <- data %>%
 bar_labels_r <- input_regional %>%
   group_by(REGION_NAME) %>%
   summarize("no_wells"=sum(count)) %>%
-  mutate("no_wells_lab" = paste0(no_wells, " wells")) %>%
+  mutate("no_wells_lab" = ifelse(no_wells>1, paste0(no_wells, " wells"), paste0(no_wells, " well"))) %>%
   mutate(prop_tot = (no_wells/length(unique(data$Well_Num)))*100) #Calculate total proportion for graph scale
 
 
@@ -108,7 +119,7 @@ ggplot(data=input_regional) +
   scale_x_continuous(expand = c(0,0)) +
   expand_limits(x=c(0,55)) +
   guides(fill = guide_legend(reverse = TRUE))+
-  labs(title = "Regional Summary") +
+  #labs(title = "Groundwater Wells by Region") +
   xlab("Proportion of Wells (%)") + ylab(NULL) +
   theme_classic() +
   theme(legend.position="bottom",
@@ -119,14 +130,19 @@ ggplot(data=input_regional) +
 
 ##########################################################
 #Define function to create groundwater level plot and trend line
-groundwater_level_plot = function(data,variable_choice,clicked_station,trend_results,slopes){
+groundwater_level_plot = function(data,period_choice,var_choice,month_choice,clicked_station,trend_results,slopes){
 
 #If no selection, print "No selection" - set as default when app opens
   if(clicked_station == 'No selection'){
       ggplot() +
-        geom_text(aes(x=1,y=1,label='Click a station on the map to see its plot.')) +
+        geom_text(aes(x=1,y=1,label='Click on a region or groundwater monitoring station on the map to see its plot.')) +
         ggthemes::theme_map()
     } else {
+
+      #If 10 years selected, filter years of data shown
+      if(period_choice == "10 Years"){
+        data <- data %>% filter(Year >= 2012)
+      }
 
       #If a well is selected (clicked on Leaflet map) - select data only for that well
       well_num = trend_results %>%
@@ -151,6 +167,86 @@ groundwater_level_plot = function(data,variable_choice,clicked_station,trend_res
       maxDate = as.Date(max(well_levels$Date))
       nYears <- as.numeric(difftime(maxDate, minDate, units = "days"))/365
 
+
+      if(var_choice == "Monthly"){
+
+        month_no <- as.numeric(match(month_choice,month.abb))
+
+        well_levels_monthly <- well_levels %>%
+          filter(Month == month_no)
+
+        #Define water level limits for plot creation
+        # maxgwl = max(ts$med_GWL, na.rm = TRUE)
+        # mingwl = min(ts$med_GWL, na.rm = TRUE)
+        # gwlrange = maxgwl - mingwl
+        # midgwl = (maxgwl + mingwl)/2
+        # lims  = c(midgwl + gwlrange, midgwl - gwlrange)
+        # ts$max_lims <- max(lims[1], max(ts$med_GWL, na.rm = TRUE) + 5)
+        # ts$colour <- ifelse(ts$nReadings ==1, "grey", "blue")
+
+
+        plot <- ggplot(data=well_levels_monthly, aes(x=Year, y=med_GWL, yend = max_lims, xend=Year)) +
+          geom_segment(aes(color = "Groundwater Level", linewidth = 0.5)) +
+          labs(x = "Year", y = "Depth Below Ground (metres)") +
+          scale_y_reverse(expand = c(0,0)) + coord_cartesian(ylim = lims) +
+          scale_x_continuous(expand = c(0,0)) +
+          theme_classic() +
+          theme(
+            text = element_text(colour = "black"),
+            panel.grid.minor.x = element_blank(),
+            panel.grid.major.x = element_blank(),
+            axis.line = element_line(colour="grey50"),
+            legend.position = "bottom", legend.box =  "horizontal",
+            plot.title = element_text(hjust = 0.5),
+            plot.subtitle = element_text(hjust = 0.5, face = "plain", size = 11)) +
+          theme(plot.margin = margin(10, 10, 10, 10, "points")) +
+          theme(legend.position="bottom",
+                legend.title=element_blank(),
+                legend.box.just ="left") +
+          scale_color_manual(breaks = c("Groundwater Level"),
+                             values = c("Groundwater Level" = "#C6DDFD"),
+                             guide=guide_legend(override.aes=list(linetype=c("solid"),
+                                                                  shape=c(NA), size=c(5)))) +
+          scale_fill_manual(labels = "Groundwater Level", values = c("Groundwater Level" = "#C6DDFD"))
+
+        #Print plots for wells without significant trends (water level and interpolated points only)
+        if(well_num$state == "Stable" | well_num$state == "Too many missing observations to perform trend analysis" |
+           well_num$state == "Recently established well; time series too short for trend analysis"){
+
+          plot }
+
+        else{
+
+          #For wells with significant trends (increasing, moderate rate of decline, or large rate of decline)
+          #Add trend line information
+          slope = as.numeric(well_num$trend_line_slope)/365/12
+          intercept = as.numeric(well_num$trend_line_int)
+          int.well = intercept + slope * as.numeric(minDate)
+
+          #If interpolated values present, add trend line to chart and both line and points to legend and print plot
+          # if(nrow(nZeroReadings)>0){
+          #   plot +
+          #     geom_abline(aes_string(intercept = "intercept", slope = "slope", colour = "'LTT'"),
+          #                 data = data.frame(intercept = -int.well, slope = slope), size = 1) +
+          #     scale_colour_manual(name = '', values = c(LTT = 'orange', Interp = 'grey60'),
+          #                         labels = c('Long-term Trend', 'Interpolated (Missing) Values'),
+          #                         guide = guide_legend(override.aes = list(colour = c("orange", "grey60"), shape = c(NA, 16), linetype = c(1, 0))))
+          # }else {
+            #If no interpolated values present, only add trend line to chart and legend and print plot
+            plot +
+              geom_abline(aes_string(intercept = "intercept", slope = "slope", colour = "'LTT'"),
+                          data = data.frame(intercept = -int.well, slope = slope), size = 1) +
+              scale_colour_manual(breaks = c('Long-term Trend', 'Groundwater Level'),
+                                  values = c(LTT = 'orange', GWL = '#C6DDFD'),
+                                  labels = c('Long-term Trend', 'Groundwater Level'),
+                                  guide=guide_legend(override.aes=list(linetype=c("solid", "solid"),
+                                                                       shape=c(NA, NA), size=c(5, 5)))) +
+              scale_fill_manual(labels = c('Long-term Trend', 'Groundwater Level'),
+                                values = c(LTT = 'orange', GWL = '#C6DDFD'))
+        }
+
+        }else{
+
       #Define base plot with only water levels
         plot <- ggplot(well_levels, aes_string(x = "Date")) +
           geom_ribbon(aes_string(ymin = "med_GWL",
@@ -158,7 +254,7 @@ groundwater_level_plot = function(data,variable_choice,clicked_station,trend_res
                                  fill = "'Groundwater Level'"), alpha = 0.3) +
           # labs(title = "Observed Long-term Trend in Groundwater Levels\n", x = "Date",
           #      y = "Depth Below Ground (metres)") +
-          labs(x = "Year", y = "Depth Below Ground (metres)") +
+          labs(x = "Date", y = "Depth Below Ground (metres)") +
           theme_minimal() +
           theme(
             text = element_text(colour = "black"),
@@ -171,12 +267,11 @@ groundwater_level_plot = function(data,variable_choice,clicked_station,trend_res
           scale_y_reverse(expand = c(0,0)) + coord_cartesian(ylim = lims) +
           scale_x_date(expand = c(0,0)) +
           #scale_x_date(labels = "Year", year("Date"),
-          #breaks = scale_x_date(dplyr::if_else(nYears < 10, #This is not working
+          # breaks = scale_x_date(dplyr::if_else(nYears < 10, #This is not working
           #                                   "1 year",
           #                                   "3 years")),
           #             expand = c(0,0)) +
           scale_fill_manual(name = '', values = c('Groundwater Level' = "#1E90FF"))
-
 
         #If interpolated values were identified, add point element to chart and legend
         if(nrow(nZeroReadings)>0){
@@ -220,5 +315,126 @@ groundwater_level_plot = function(data,variable_choice,clicked_station,trend_res
           scale_colour_manual(name = '', values = c(LTT = 'orange', Interp = 'grey60'),
                               labels = c('Long-term Trend', 'Interpolated (Missing) Values'))
       }}
+      }
     }
 }
+
+#################################################
+# #Define function to update region identification text
+#
+# regional_text_function <- function(region_data, region_name){
+#
+#   #Count missing wells
+#   well_state_count <- region_data %>%
+#     group_by(state) %>%
+#     summarize("count" = n()) %>%
+#     full_join(state_list, by=c("state"="unique(results_out$state)")) %>%
+#     mutate_all(~replace(., is.na(.), 0))
+#
+#   recent_cnt <- as.character(well_state_count[5,2])
+#   missing_cnt <- as.character(well_state_count[6,2])
+#
+#   #Report selected region
+#
+#     HTML(paste0("<div style='background-color:white; padding: 8px'>",
+#                 "<strong>Natural Resource Region: ", region_name,
+#                 "</strong> <br>",
+#                 "Count of recent wells: ", recent_cnt, "<br>",
+#                 "Count of wells with missing data: ", missing_cnt, "</div"))}
+
+#################################################
+#Function to select state based on input filters
+
+sf_filter <- function(period, time_scale, month){
+
+  if(period == "Yearly"){
+  col <- case_when(time_scale == "All" ~ 16,
+                    time_scale == "10 Years" ~ 17)
+
+  return(col)
+
+  }
+
+  if(period == "Monthly"){
+    col <- case_when(time_scale == "All" & month == "Jan" ~ 18,
+                       time_scale == "10 Years" & month == "Jan" ~ 19,
+                       time_scale == "All" & month == "Feb" ~ 20,
+                       time_scale == "10 Years" & month == "Feb" ~ 21,
+                       time_scale == "All" & month == "Mar" ~ 22,
+                       time_scale == "10 Years" & month == "Mar" ~ 23,
+                       time_scale == "All" & month == "Apr" ~ 24,
+                       time_scale == "10 Years" & month == "Apr" ~ 25,
+                       time_scale == "All" & month == "May" ~ 26,
+                       time_scale == "10 Years" & month == "May" ~ 27,
+                       time_scale == "All" & month == "Jun" ~ 28,
+                       time_scale == "10 Years" & month == "Jun" ~ 29,
+                       time_scale == "All" & month == "Jul" ~ 30,
+                       time_scale == "10 Years" & month == "Jul" ~ 31,
+                       time_scale == "All" & month == "Aug" ~ 32,
+                       time_scale == "10 Years" & month == "Aug" ~ 33,
+                       time_scale == "All" & month == "Sep" ~ 34,
+                       time_scale == "10 Years" & month == "Sep" ~ 35,
+                       time_scale == "All" & month == "Oct" ~ 36,
+                       time_scale == "10 Years" & month == "Oct" ~ 37,
+                       time_scale == "All" & month == "Nov" ~ 38,
+                       time_scale == "10 Years" & month == "Nov" ~ 39,
+                       time_scale == "All" & month == "Dec" ~ 40,
+                       time_scale == "10 Years" & month == "Dec" ~ 41,)
+
+    return(col)
+
+}
+
+
+}
+
+#Monthly plot function
+
+# data <- results_out %>%
+#   filter(period == "All", time_scale == "Monthly", Well_Num == 262) %>%
+#   filter(month == "Jan") %>%
+#   mutate(month_no = as.numeric(match(month,month.abb)))
+#
+#
+# ts <- monthlywells_ts %>%
+#   filter(Well_Num == data$Well_Num, Month == data$month_no)
+#
+# #Define water level limits for plot creation
+# maxgwl = max(ts$med_GWL, na.rm = TRUE)
+# mingwl = min(ts$med_GWL, na.rm = TRUE)
+# gwlrange = maxgwl - mingwl
+# midgwl = (maxgwl + mingwl)/2
+# lims  = c(midgwl + gwlrange, midgwl - gwlrange)
+# ts$max_lims <- max(lims[1], max(ts$med_GWL, na.rm = TRUE) + 5)
+# ts$colour <- ifelse(ts$nReadings ==1, "grey", "blue")
+#
+# ggplot(data=ts, aes(x=Year, y=med_GWL, yend = max_lims, xend=Year)) +
+#   geom_segment(aes(color = "Groundwater Level", linewidth = 0.5)) +
+#   labs(x = "Year", y = "Depth Below Ground (metres)") +
+#   theme(
+#     text = element_text(colour = "black"),
+#     panel.grid.minor.x = element_blank(),
+#     panel.grid.major.x = element_blank(),
+#     axis.line = element_line(colour="grey50"),
+#     legend.position = "bottom", legend.box =  "horizontal",
+#     plot.title = element_text(hjust = 0.5),
+#     plot.subtitle = element_text(hjust = 0.5, face = "plain", size = 11)) +
+#   scale_y_reverse(expand = c(0,0)) + coord_cartesian(ylim = lims) +
+#   scale_x_continuous(expand = c(0,0)) +
+#   theme_classic() +
+#   theme(legend.position="bottom",
+#         legend.title=element_blank(),
+#         legend.box.just ="left") +
+#   scale_color_manual(breaks = c("Groundwater Level"),
+#                      values = c("Groundwater Level" = "#C6DDFD"),
+#                      guide=guide_legend(override.aes=list(linetype=c("solid"),
+#                                                           shape=c(NA), size=c(5)))) +
+#   scale_fill_manual(labels = "Groundwater Level", values = c("Groundwater Level" = "#C6DDFD"))
+#
+#
+# test <- ts %>%    geom_col(mapping=aes(x=Year, y=REGION_NAME, fill=colour, width = 0.5))value
+#   geom_bar(stat = 'identity')+
+#   #geom_text(aes(label=label),vjust=-0.5,fontface='bold')+
+#   scale_fill_manual(values = c('tomato','cyan3','magenta',
+#                                'transparent','transparent','transparent'))+
+#   theme(legend.position = 'none')
