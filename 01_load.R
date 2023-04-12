82fb11d1252b597d5760f0e03bae40378e4133fc
Karly Harker <54858492+KarHarker@users.noreply.github.com>
2023-04-12 10:57:52 -0700

    Code clean-up, hard-coding bcdata id, fixing repetitive console outputs

--- 01_load.R
+++ 01_load.R
@@ -1,4 +1,4 @@
-# Copyright 2018 Province of British Columbia
+# Copyright 2023 Province of British Columbia
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@
 # https://catalogue.data.gov.bc.ca/dataset/e4731a85-ffca-4112-8caf-cb0a96905778
 # Natural Resource (NR) Regions: 
 # https://catalogue.data.gov.bc.ca/dataset/dfc492c0-69c5-4c20-a6de-2c9bc999301f
-# Here we automatically grab the NR Regions using the 'bcmaps' R package 
 ########################################################################################
 
 
@@ -37,20 +36,19 @@ source("func.R")
 
 if(!dir.exists('data'))dir.create("data", showWarnings = FALSE)
 
-## CHRIS ADDITION - START ##
-
-# Original download link broken. Attempting to get data directly from BC Data Catalogue.
-obs_wells_in = bcdata::bcdc_query_geodata('groundwater-wells') %>% 
+# Retrieve data directly from BC Data Catalogue.
+obs_wells_in = bcdata::bcdc_get_data('e4731a85-ffca-4112-8caf-cb0a96905778') %>% 
   filter(!is.na(WELL_TAG_NUMBER),
          !is.na(WELL_STATUS)) %>% 
   collect() %>% 
   setNames(snakecase::to_snake_case(colnames(.)))
 
+##Filter out observations with no observation well status or number (the vast majority!)
+#Join the natural resource region names etc. to data
+
 obs_wells = obs_wells_in %>% 
-  ##Filter out observations with no observation well status or number (the vast majority!)
   # filter(!is.na(observation_well_status)) %>% 
   # filter(!is.na(observation_well_number)) %>% 
-  #Join the natural resource region names etc. to data
   st_join(nr_regions()) %>% 
   #Make column names 'R-friendly'
   setNames(snakecase::to_snake_case(colnames(.))) %>% 
@@ -77,9 +75,17 @@ obs_wells = obs_wells_in %>%
 # If the url for a given well doesn't work, the code creates an empty data.frame to 
 # make sure such a result can still be combined with the successful data reading attempts. In this way,
 # the function does not break if one or more wells has no available data to download.
+
+col_types <- cols(
+  Time = col_datetime(format = "POSIXct"),
+  Value = col_double(),
+  Approval = col_character(),
+  myLocation = col_character()
+)
+
 wells_data_raw = obs_wells$observation_well_number %>% 
   map( ~ {
-    tryCatch(read_csv(paste0("https://www.env.gov.bc.ca/wsd/data_searches/obswell/map/data/OW",.x,"-data.csv")), 
+    tryCatch(read_csv(paste0("https://www.env.gov.bc.ca/wsd/data_searches/obswell/map/data/OW",.x,"-data.csv"), col_types = col_types), 
              #If the url for this repetition doesn't exist / work, we create an empty data.frame to 
              # make sure such a result can still be combined with the successful data reading attempts.
              error = function(e) data.frame(Time = structure(numeric(0), 
@@ -91,8 +97,6 @@ wells_data_raw = obs_wells$observation_well_number %>%
     
   }) %>% 
   bind_rows()
-
-## CHRIS ADDITION - END ##
 
 ## Save raw data objects in a temporary directory
 save(obs_wells, file = "./tmp/clean_attr_data.RData")
